5c824ca0deca76185f842a2ec04cb72f4c542b1f
abacusagent <malcolmvosen@gmail.com>
2024-11-24 19:18:58 +0000

    Great chat with Andre about the coding projects - and class objects. Alot of progress has been made, but still so much to look forward to.

--- player.py
+++ player.py
@@ -7,6 +7,7 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.timer = 0
 
     # in the player class
     def triangle(self):
@@ -28,7 +29,8 @@ class Player(CircleShape):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
     
-    def update(self, dt):    
+    def update(self, dt):
+        self.timer -= dt    
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
@@ -47,6 +49,11 @@ class Player(CircleShape):
             self.shoot()
 
     def shoot(self):
+        if self.timer > 0:
+            return
         shot = Shot(self.position.x, self.position.y)
-        shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOT_SPEED
+        shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+        self.timer = PLAYER_SHOOT_COOLDOWN
+
+        
 
