c0e9b020c5e657619e6ccbaf09f473a41eedc07a
abacusagent <malcolmvosen@gmail.com>
2024-11-24 08:10:12 +0000

    You've added in a fps clocker and have submitted other changes.

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
+    Clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while 2 == 2:
@@ -16,6 +18,8 @@ def main():
         colour = (0, 0, 0)
         display = pygame.Surface.fill(screen, colour)
         pygame.display.flip()
+        fps = Clock.tick(60)
+        dt = (fps / 1000)
     return display
 
 
